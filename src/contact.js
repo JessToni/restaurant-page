51abd55416aaaee5d2269f02e7a5284626d79375
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-11-03 09:46:15 +0800

    Add details to menu and contact tab

--- src/contact.js
+++ src/contact.js
@@ -8,6 +8,16 @@ const createContactPage = () => {
     headline.textContent = 'Contact';
     pageContent.appendChild(headline);
 
+    //Establish contact details
+    const number = document.createElement('h3');
+    number.textContent = 'You can call us at 09123456789';
+    pageContent.appendChild(number);
+
+    //Establish email details
+    const email = document.createElement('h3');
+    email.textContent = 'Or send as an email at fivestardining@goodfood.com';
+    pageContent.appendChild(email);
+
     content.appendChild(pageContent);
 }
 
--- src/menu.js
+++ src/menu.js
@@ -8,6 +8,20 @@ const createMenuPage = () => {
     headline.textContent = 'Menu';
     pageContent.appendChild(headline);
 
+    //Establish list for menu items
+    const menuList = document.createElement('ul');
+    const menuItem1 = document.createElement('li');
+    const menuItem2 = document.createElement('li');
+    const menuItem3 = document.createElement('li');
+    menuItem1.textContent = "Crispy Lechon";
+    menuItem2.textContent = "Royal Dinuguan";
+    menuItem3.textContent = "Lumpia con Keso";
+    menuList.appendChild(menuItem1);
+    menuList.appendChild(menuItem2);
+    menuList.appendChild(menuItem3);
+
+    //Append elements to page content div
+    pageContent.appendChild(menuList);
     content.appendChild(pageContent);
 }
 
